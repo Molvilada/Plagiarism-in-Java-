817999b9939ff99705e78b98bfd3ab06b13b02a4
Cristhian <cgcontrerasm@unal.edu.co>
2019-07-20 16:38:58 -0500

    problems merge solved

--- src/GUI.java
+++ src/GUI.java
@@ -3,21 +3,20 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 public class GUI {
-    private JButton realizarAnálisisButton;
+    private JButton realizarAnalisisButton;
     private JComboBox comboBox1;
     private JTextArea codigoAAnalizar1TextArea;
     private JTextArea codigoAAnalizar2TextArea;
-    private JButton button1;
-
+    private JPanel Jpanel;
     public GUI() {
-        realizarAnálisisButton.addMouseListener(new MouseAdapter() {
-        });
-        realizarAnálisisButton.addMouseListener(new MouseAdapter() {
+        realizarAnalisisButton.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 String entrada1 = codigoAAnalizar1TextArea.getText();
                 String entrada2 = codigoAAnalizar2TextArea.getText();
+                String caso = (String) comboBox1.getSelectedItem();
                 System.out.println(entrada1);
+                System.out.println(caso);
                 System.out.println(entrada2);
             }
         });
@@ -29,6 +28,9 @@ public class GUI {
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Analizador");
-        frame.setContentPane(new GUI().comboBox1);
+        frame.setContentPane(new GUI().Jpanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
