19cfb359005f2f632a86de3c24c27dbe39eec6bd
Laura Molna <ldmolinav@unal.edu.co>
2019-07-21 14:37:36 -0500

    add for and while

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,41 @@ import java.util.Collections;
 
 public class Main {
 
-    static void CompararForFor(Plagiarism plagiarism1, Plagiarism plagiarism2){
-        int foriguales = 0;
-        String[] tokens = new String[3];
+    static void CompararFor1While2(ArrayList<ArrayList<String>> tokensString1fors, ArrayList<ArrayList<String>> tokensString2whiles
+    ,ArrayList<ArrayList<Integer>> tokensTypes1fors, ArrayList<ArrayList<Integer>> tokensType2whiles){
+        ArrayList<ArrayList> info_for = new ArrayList<>();
+        ArrayList<ArrayList> info_while = new ArrayList<>();
+        ArrayList<String> temporal_for = new ArrayList<>();
+        ArrayList<String> temporal_while = new ArrayList<>();
+        for (int i = 0; i < tokensString1fors.size(); i++){
+            for(int j = 3; j < 13; j++){
+                temporal_for.add(tokensString1fors.get(i).get(j));
+            }
+            info_for.add(new ArrayList(temporal_for));
+            temporal_for.clear();
+        }
+
+        for (int i = 0; i < tokensString2whiles.size(); i++){
+            temporal_while.add(tokensString2whiles.get(i).get(tokensString2whiles.get(i).size() - 3));
+            temporal_while.add(tokensString2whiles.get(i).get(tokensString2whiles.get(i).size() - 2));
+            temporal_while.add(tokensString2whiles.get(i).get(tokensString2whiles.get(i).size() - 1));
+            temporal_while.add(";");
+            temporal_while.add(tokensString2whiles.get(i).get(2));
+            temporal_while.add(tokensString2whiles.get(i).get(3));
+            temporal_while.add(tokensString2whiles.get(i).get(4));
+            temporal_while.add(";");
+            for (int j = 6; j < tokensString2whiles.get(i).size(); j++){
+                if(tokensString2whiles.get(i).get(j).equals(temporal_while.get(0)) && (tokensString2whiles.get(i).get(j+1).equals("++") || tokensString2whiles.get(i).get(j+1).equals("--"))){
+                    temporal_while.add(tokensString2whiles.get(i).get(j));
+                    temporal_while.add(tokensString2whiles.get(i).get(j+1));
+                    break;
+                }
+            }
+            info_while.add(new ArrayList(temporal_while));
+            temporal_while.clear();
+        }
+
+
 
     }
 
@@ -44,13 +76,32 @@ public class Main {
 //            System.out.println(tree2.toStringTree(parser2)); // imprime el arbol al estilo LISP
             ArrayList<String> tokensString1 = new ArrayList<>();
             ArrayList<ArrayList<String>> tokensString1fors = new ArrayList<>();
-            ArrayList<ArrayList<String>> tokensType1fors = new ArrayList<>();
+            ArrayList<ArrayList<Integer>> tokensType1fors = new ArrayList<>();
+            ArrayList<ArrayList<String>> tokensString1whiles = new ArrayList<>();
+            ArrayList<ArrayList<Integer>> tokensType1whiles = new ArrayList<>();
             ArrayList<String> tokensString2 = new ArrayList<>();
+            ArrayList<ArrayList<String>> tokensString2fors = new ArrayList<>();
+            ArrayList<ArrayList<Integer>> tokensType2fors = new ArrayList<>();
+            ArrayList<ArrayList<String>> tokensString2whiles = new ArrayList<>();
+            ArrayList<ArrayList<Integer>> tokensType2whiles = new ArrayList<>();
             ArrayList<Integer> tokensType1 = new ArrayList<>();
             ArrayList<Integer> tokensType2 = new ArrayList<>();
             ArrayList<String> temporal = new ArrayList<>();
+            ArrayList<Integer> temporal2 = new ArrayList<>();
+
             int k = 0;
-            int bandera_for = 0, llaves = 0;
+            int bandera_for = 0, llaves = 0, bandera_while = 0, bandera_variable = 0;
+            ArrayList<String> variables1 = new ArrayList<>();
+            ArrayList<String> variables2 = new ArrayList<>();
+            ArrayList<Integer> posicion1 = new ArrayList<>();
+            ArrayList<Integer> posicion2 = new ArrayList<>();
+
+            if(tokens1.getTokens().size() < tokens2.getTokens().size()){
+                CommonTokenStream tokens_t = new CommonTokenStream(lexer1);
+                tokens1 = tokens2;
+                tokens2 = tokens_t;
+            }
+
             for(Object c : tokens1.getTokens()){
                 Token t = (Token) c;
                 tokensString1.add(t.getText());
@@ -58,8 +109,14 @@ public class Main {
                 if (t.getText().equals("for")){
                     bandera_for = 1;
                 }
+                if (t.getText().equals("while")){
+                    bandera_while = 1;
+                    bandera_variable = 0;
+                    posicion1.add(t.getTokenIndex());
+                }
                 if (bandera_for > 0){
                     temporal.add(t.getText());
+                    temporal2.add(t.getType());
                     if (t.getText().equals("{")){
                         llaves++;
                     }
@@ -67,20 +124,120 @@ public class Main {
                         llaves--;
                         if(llaves == 0){
                             tokensString1fors.add(new ArrayList<>(temporal));
+                            tokensType1fors.add(new ArrayList<>(temporal2));
                             temporal.clear();
+                            temporal2.clear();
                             bandera_for = 0;
+                        }
+
+                    }
+                }
+                if (bandera_while > 0){
+                    if(t.getType() == 102 && bandera_variable == 0){
+                        variables1.add(t.getText());
+                        bandera_variable++;
+                    }
+                    temporal.add(t.getText());
+                    temporal2.add(t.getType());
+                    if (t.getText().equals("{")){
+                        llaves++;
+                    }
+                    if (t.getText().equals("}")){
+                        llaves--;
+                        if(llaves == 0){
+                            tokensString1whiles.add(new ArrayList<>(temporal));
+                            tokensType1whiles.add(new ArrayList<>(temporal2));
+                            temporal.clear();
+                            temporal2.clear();
+                            bandera_while = 0;
                         }
 
                     }
                 }
             }
 
-            System.out.println(tokensString1fors.toString());
+            bandera_for = 0; llaves = 0; bandera_while = 0; bandera_variable = 0;
+
             for(Object c : tokens2.getTokens()){
                 Token t = (Token) c;
                 tokensString2.add(t.getText());
                 tokensType2.add(t.getType());
+                if (t.getText().equals("for")){
+                    bandera_for = 1;
+                }
+                if (t.getText().equals("while")){
+                    bandera_while = 1;
+                    bandera_variable = 0;
+                    posicion2.add(t.getTokenIndex());
+                }
+                if (bandera_for > 0){
+                    temporal.add(t.getText());
+                    temporal2.add(t.getType());
+                    if (t.getText().equals("{")){
+                        llaves++;
+                    }
+                    if (t.getText().equals("}")){
+                        llaves--;
+                        if(llaves == 0){
+                            tokensString2fors.add(new ArrayList<>(temporal));
+                            tokensType2fors.add(new ArrayList<>(temporal2));
+                            temporal.clear();
+                            temporal2.clear();
+                            bandera_for = 0;
+                        }
+
+                    }
+                }
+                if (bandera_while > 0){
+                    if(t.getType() == 102 && bandera_variable == 0){
+                        variables2.add(t.getText());
+                        bandera_variable++;
+                    }
+                    temporal.add(t.getText());
+                    temporal2.add(t.getType());
+                    if (t.getText().equals("{")){
+                        llaves++;
+                    }
+                    if (t.getText().equals("}")){
+                        llaves--;
+                        if(llaves == 0){
+                            tokensString2whiles.add(new ArrayList<>(temporal));
+                            tokensType2whiles.add(new ArrayList<>(temporal2));
+                            temporal.clear();
+                            temporal2.clear();
+                            bandera_while = 0;
+                        }
+
+                    }
+                }
             }
+
+            for(int i = 0; i < variables1.size(); i++){
+                for(int j = posicion1.get(i); j > 0; j--){
+                    if (tokensString1.get(j).equals(variables1.get(i))){
+                        tokensString1whiles.get(i).add(tokensString1.get(j));
+                        tokensString1whiles.get(i).add(tokensString1.get(j + 1));
+                        tokensString1whiles.get(i).add(tokensString1.get(j + 2));
+                    }
+                }
+
+            }
+
+            for(int i = 0; i < variables2.size(); i++){
+                for(int j = posicion2.get(i); j > 0; j--){
+                    if (tokensString2.get(j).equals(variables2.get(i))){
+                        tokensString2whiles.get(i).add(tokensString2.get(j));
+                        tokensString2whiles.get(i).add(tokensString2.get(j + 1));
+                        tokensString2whiles.get(i).add(tokensString2.get(j + 2));
+                    }
+                }
+
+            }
+
+//            System.out.println(tokensString1whiles.toString());
+//            System.out.println(variables1.toString());
+//            System.out.println(tokensString2whiles.toString());
+//            System.out.println(variables2.toString());
             Collections.sort(tokensString1);
             Collections.sort(tokensString2);
             Collections.sort(tokensType1);
@@ -129,7 +286,7 @@ public class Main {
             System.out.println("Tipos de tokens iguales: " + iguales_type);
             System.out.println("Jaccard Tokens:  " + ((float)(iguales_text)/(float)(size1 + size2-iguales_text)));
             System.out.println("Jaccard tipos de tokens:  " + ((float)(iguales_type)/(float)(size1 + size2-iguales_type)));
-            CompararForFor(analisis1, analisis2);
+            CompararFor1While2(tokensString1fors, tokensString2whiles, tokensType1fors, tokensType2whiles);
         } catch (Exception e){
             System.err.println("Error (Test): " + e);
         }
